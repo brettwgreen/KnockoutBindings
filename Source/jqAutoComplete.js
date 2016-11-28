5cf9015da0b38087300ede63cfdb2723fc0f24bf
Brett Green <bgreen@fdsrx.com>
2016-11-28 15:12:03 -0500

    more functional

--- Source/jqAutoComplete.js
+++ Source/jqAutoComplete.js
@@ -2,7 +2,13 @@ ko.bindingHandlers.jqAutoComplete = {
     init: function(element, valueAccessor, allBindings) {
         var va = valueAccessor();
         var source = allBindings.has('source') ? ko.unwrap(allBindings.get('source')) : [];
-        $(element).autocomplete({source: source});
+        $(element).autocomplete({
+            source: source,
+            select: function(event, ui) {
+                va(ui.item.value);
+            }
+            });
+        
     },
     update: function(element, valueAccessor, allBindings) {
     }
