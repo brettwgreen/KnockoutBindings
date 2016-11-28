a4efae3a5d7f7a9cc6473badd4a006851416d346
Brett Green <bgreen@fdsrx.com>
2016-11-28 13:11:15 -0500

    better messaging/handling for jquery UI presence

--- Source/transitionVisible.js
+++ Source/transitionVisible.js
@@ -3,52 +3,85 @@ ko.bindingHandlers.transitionVisible = {
         var value = valueAccessor();
         var visible = ko.unwrap(value);
         if (value) {
-          $(element).show();
+            $(element).show();
         }
         else {
-          $(element).hide();
+            $(element).hide();
         }
     },
     update: function(element, valueAccessor, allBindings) {
-      // Initialize without toggling on initial binding
-      var initialized = ko.utils.domData.get(element, 'initialized');
-	  // By only examining the bound visible property on first call, it means knockout will
-	  // only call update for that bound property, and not on any of the bindable effect properties... 		 
-      var value = valueAccessor();
-      var visible = ko.unwrap(value);
-      if (initialized) {
-        var effect = allBindings.has('effect') ? ko.unwrap(allBindings.get('effect')) : 'fade';
-        var duration = allBindings.has('duration') ? ko.unwrap(allBindings.get('duration')) : 500;
-        var effectOptions = allBindings.has('effectOptions') ? ko.unwrap(allBindings.get('effectOptions')) : {easing: 'swing'};		
-		var easing = effectOptions.easing ? effectOptions.easing : 'swing';
-		switch (effect) {
-          case "fade":
-            $(element).fadeToggle(duration, easing);
-            break;
-          case "slide":
-            $(element).slideToggle(duration, easing);
-            break;
-		  // These require jQuery UI
-		  case "blind":
-		  case "bounce":
-		  case "clip":
-		  case "drop":
-		  case "explode":
-		  case "fold":
-		  case "highlight":
-		  case "puff":
-		  case "pulsate":
-		  case "scale":
-		  case "shake":
-		  case "size":
-            $(element).toggle(effect, effectOptions, duration);
-            break;
-          default:
-            $(element).fadeToggle(duration, easing);
-            break;
+        // Initialize without toggling on initial binding
+        var initialized = ko.utils.domData.get(element, 'initialized');
+        // By only examining the bound visible property on first call, it means knockout will
+        // only call update for that bound property, and not on any of the bindable effect properties... 		 
+        var value = valueAccessor();
+        var visible = ko.unwrap(value);
+        if (initialized) {
+            var effect = allBindings.has('effect') ? ko.unwrap(allBindings.get('effect')) : 'fade';
+            var duration = allBindings.has('duration') ? ko.unwrap(allBindings.get('duration')) : 500;
+            var effectOptions = allBindings.has('effectOptions') ? ko.unwrap(allBindings.get('effectOptions')) : {easing: 'swing'};		
+            var easing = effectOptions.easing ? effectOptions.easing : 'swing';
+            if (!jQuery.ui) {
+                switch (effect) {
+                    case "fade":
+                        $(element).fadeToggle(duration, easing);
+                        break;
+                    case "slide":
+                        $(element).slideToggle(duration, easing);
+                        break;
+                    case "blind":
+                    case "bounce":
+                    case "clip":
+                    case "drop":
+                    case "explode":
+                    case "fold":
+                    case "highlight":
+                    case "puff":
+                    case "pulsate":
+                    case "scale":
+                    case "shake":
+                    case "size":
+                        if (window.console) {
+                            console.log("No jQueryUI present, so no support for '" + effect + "'. Defaulting to fade effect.");
+                        }
+                        $(element).fadeToggle(duration, easing);
+                        break;
+                    default:
+                        if (window.console) {
+                            console.log("Unkwown effect '" + effect + "'. Defaulting to fade effect.");
+                        }
+                        $(element).fadeToggle(duration, easing);
+                    break;                
+                }
+            }
+            if (jQuery.ui) {
+                switch (effect) {
+                    case "fade":
+                    case "slide":
+                    case "blind":
+                    case "bounce":
+                    case "clip":
+                    case "drop":
+                    case "explode":
+                    case "fold":
+                    case "highlight":
+                    case "puff":
+                    case "pulsate":
+                    case "scale":
+                    case "shake":
+                    case "size":
+                        $(element).toggle(effect, effectOptions, duration);
+                        break;
+                    default:
+                        if (window.console) {
+                            console.log("Unkwown effect '" + effect + "'. Defaulting to fade effect.");
+                        }
+                        $(element).fadeToggle(duration, easing);
+                        break;
+                }
+            }
         }
-      }
-      ko.utils.domData.set(element, 'initialized', 'true');
+        ko.utils.domData.set(element, 'initialized', 'true');
     }
 };
- 
+
