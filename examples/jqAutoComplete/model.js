becd7c04067a56a11b27712c67ae52d73888bbce
Brett Green <bgreen@fdsrx.com>
2016-11-28 15:21:34 -0500

    observable source example

--- examples/jqAutoComplete/model.js
+++ examples/jqAutoComplete/model.js
@@ -2,14 +2,19 @@ $(document).ready(function() {
     var ViewModel = function() {
         var self = this;
         self.selectedCharacter = ko.observable("");
-        self.characters = [
+        self.newCharacter = ko.observable("");
+        
+        self.characters = ko.observableArray([
             "Bugs Bunny",
             "Elmer Fudd",
             "Marvin Martian",
             "Roadrunner",
             "Wile E. Coyote"
-         ];
-         
+         ]);
+        self.addCharacter = function(name) {
+            self.characters.push(self.newCharacter());
+        };
+        
     };
     
     var vm = new ViewModel;
